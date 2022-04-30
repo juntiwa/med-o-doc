3a8bd015ec4b69b267df4476000a68047d0476c4
juntiwa <juntima.nuch@gmail.com>
2022-04-30 17:40:18 +0700

    magin heading page

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,10 @@
 
 use App\Http\Controllers\Auth\LoginController as AuthLoginController;
 use App\Http\Controllers\Admin\ActivityLogController as AdminActivitylogController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RecController;
 use App\Http\Controllers\RegController;
 use App\Http\Controllers\SendController;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
