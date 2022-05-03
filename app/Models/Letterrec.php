bcd012d2f1d34a1f5a28d9e41c5fa82f6dad36ca
juntiwa <juntima.nuch@gmail.com>
2022-05-03 20:56:24 +0700

    modified aliases

--- app/Models/Letterrec.php
+++ app/Models/Letterrec.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Letterrec extends Model
 {
    use HasFactory;
+
+   public function thaidate()
+   {
+      return Carbon::parse($this->recdate)->thaidate();
+   }
 
    // สำหรับ regtitle
    public function letterregs()
