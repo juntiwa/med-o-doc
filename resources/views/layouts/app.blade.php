1c421f2e3e9c21cc980c2a5f37d888c72ad1aac3
juntiwa <juntima.nuch@gmail.com>
2022-05-17 08:57:02 +0700

    change head web

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta name="csrf-token" content="{{ csrf_token() }}">
-   <title>@yield('title') - ระบบค้นหาเอกสารเก่า</title>
+   <title>@yield('title') - MED O-Doc</title>
    <link rel="icon" href="{{asset('images/search.png')}}" type="image/x-icon">
 
    <!-- unicons -->
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,7 +7,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-   <title>@yield('title') - ระบบค้นหาเอกสารเก่า</title>
+   <title>@yield('title') - MED O-Doc</title>
 
    <!-- Styles -->
    <link rel="stylesheet" href="{{asset('css/app.css')}}">
