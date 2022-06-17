1e3e8f51e032d3b5174e913ddef724426af599a3
juntiwa <juntima.nuch@gmail.com>
2022-06-17 14:41:39 +0700

    display unit name

--- app/Models/Lettersend.php
+++ app/Models/Lettersend.php
@@ -8,86 +8,94 @@ use Illuminate\Support\Carbon;
 
 class Lettersend extends Model
 {
-   use HasFactory;
+    use HasFactory;
 
-   protected $fillable = [
-      'regrecid'
+    protected $fillable = [
+      'regrecid',
    ];
 
-   public function thaidate()
-   {
-      return Carbon::parse($this->senddate)->thaidate();
-   }
+    public function thaidate()
+    {
+        return Carbon::parse($this->senddate)->thaidate();
+    }
 
-   public function thairecdate()
-   {
-      return Carbon::parse($this->recdate)->thaidate();
-   }
+    public function thairecdate()
+    {
+        return Carbon::parse($this->recdate)->thaidate();
+    }
 
-   // สำหรับ regtitle
-   public function letterregs()
-   {
-      return $this->hasMany(Letterreg::class, 'regrecid', 'regrecid');
-   }
+    // สำหรับ regtitle
+    public function letterregs()
+    {
+        return $this->hasMany(Letterreg::class, 'regrecid', 'regrecid');
+    }
 
-   //  type 1 เรื่อง มีได้ 1 ภาคเท่านั้น
-   public function types()
-   {
-      return $this->hasOne(Type::class, 'typeid', 'sendtype');
-   }
+    //  type 1 เรื่อง มีได้ 1 ภาคเท่านั้น
+    public function types()
+    {
+        return $this->hasOne(Type::class, 'typeid', 'sendtype');
+    }
 
-   //  from ส่งจากที่ไหน 1 เรื่องสามารถส่งได้หลายที่
-   public function fromins()
-   {
-      return $this->hasMany(Jobunit::class, 'unitid', 'sendunitid');
-   }
-   public function fromouts()
-   {
-      return $this->hasMany(Letterunit::class, 'unitid', 'sendunitid');
-   }
-   
-   // to ส่งถึงที่ไหน 1 เรื่องสามารถส่งถึงได้หลายที่
-   public function toins()
-   {
-      return $this->hasMany(Jobunit::class, 'unitid', 'sendtoid');
-   }
-   public function toouts()
-   {
-      return $this->hasMany(Letterunit::class, 'unitid', 'sendtoid');
-   }
+    //  from ส่งจากที่ไหน 1 เรื่องสามารถส่งได้หลายที่
+    public function fromins()
+    {
+        return $this->hasMany(Jobunit::class, 'unitid', 'sendunitid');
+    }
 
-   public function getSenddocUrlAttribute()
-   {
-      $year = substr($this->regdoc, 0, 4);
-      return url("/open-files/{$year}/{$this->regrecid}");
-   }
+    public function fromouts()
+    {
+        return $this->hasMany(Letterunit::class, 'unitid', 'sendunitid');
+    }
 
-   public function getSenddoc2UrlAttribute()
-   {
-      $year = substr($this->regdoc2, 0, 4);
-      return url("/open-files2/{$year}/{$this->regrecid}");
-   }
+    // to ส่งถึงที่ไหน 1 เรื่องสามารถส่งถึงได้หลายที่
+    public function toins()
+    {
+        return $this->hasMany(Jobunit::class, 'unitid', 'sendtoid');
+    }
 
-   // description
-   // to ส่งถึงที่ไหน 1 เรื่องสามารถส่งถึงได้หลายที่
-   public function destoins()
-   {
-      return $this->hasMany(Jobunit::class, 'unitid', 'sendtoid');
-   }
-   public function destoouts()
-   {
-      return $this->hasMany(Letterunit::class, 'unitid', 'sendtoid');
-   }
+    public function toouts()
+    {
+        return $this->hasMany(Letterunit::class, 'unitid', 'sendtoid');
+    }
 
-   public function thaidateregdate(){
-      return Carbon::parse($this->regdate)->thaidate();
-   }
+    public function getSenddocUrlAttribute()
+    {
+        $year = substr($this->regdoc, 0, 4);
 
-   public function thaidatesenddate(){
-      return Carbon::parse($this->senddate)->thaidate();
-   }
+        return url("/open-files/{$year}/{$this->regrecid}");
+    }
 
-   public function thaidaterecdate(){
-      return Carbon::parse($this->recdate)->thaidate();
-   }
+    public function getSenddoc2UrlAttribute()
+    {
+        $year = substr($this->regdoc2, 0, 4);
+
+        return url("/open-files2/{$year}/{$this->regrecid}");
+    }
+
+    // description
+    // to ส่งถึงที่ไหน 1 เรื่องสามารถส่งถึงได้หลายที่
+    public function destoins()
+    {
+        return $this->hasMany(Jobunit::class, 'unitid', 'sendtoid');
+    }
+
+    public function destoouts()
+    {
+        return $this->hasMany(Letterunit::class, 'unitid', 'sendtoid');
+    }
+
+    public function thaidateregdate()
+    {
+        return Carbon::parse($this->regdate)->thaidate();
+    }
+
+    public function thaidatesenddate()
+    {
+        return Carbon::parse($this->senddate)->thaidate();
+    }
+
+    public function thaidaterecdate()
+    {
+        return Carbon::parse($this->recdate)->thaidate();
+    }
 }
