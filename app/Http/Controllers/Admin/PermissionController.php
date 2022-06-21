f8549bf2ea4550b2ae52196d38fe3547801d442e
juntiwa <juntima.nuch@gmail.com>
2022-06-21 12:26:27 +0700

    add middleware to controller admin

--- app/Http/Controllers/Admin/PermissionController.php
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -15,6 +15,16 @@ use Illuminate\Support\Facades\URL;
 class PermissionController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware(['auth', 'admin']);
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
