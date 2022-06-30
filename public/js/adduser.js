8ca2b6390f08f1ea9f76f8ef3b3120246bc5010f
juntiwa <juntima.nuch@gmail.com>
2022-06-30 09:46:08 +0700

    review code permission user error message

--- public/js/adduser.js
+++ public/js/adduser.js
@@ -51,12 +51,12 @@ $(document).ready(function () {
       }
    });
    
-
    // show username when input sapid
-   $('#sapid1').change(function () {
-      let sapid = $(this).val();
+   $('input[name=sapid1]').change(function () {
+      var sapid = $(this).val();
+      // console.log(sapid)
       if (sapid !== '') {
-         $('#permission').prop('disabled', false);
+         $('#permission1').prop('disabled', false)
          $.ajax({  
             url: "/check-sapid",
             type: 'post',
@@ -65,10 +65,195 @@ $(document).ready(function () {
             },
             success: function (result) {
                // $('#idfrom').val(ui.item.unitid);
-               $('#username1').val(result.AccountName);
-               console.log(result);
+               if (result.Status == 'Active') {
+                  $('#username1').val(result.AccountName);
+                  $('#username1').addClass('disabled:text-teal-500');
+               } else {
+                  $('#username1').val(result.AccountName +' สถานะ '+ result.Status);
+                  $('#username1').addClass('disabled:text-red-500');
+               }
+               // console.log(result.Status);
             }
-         });
+         })
+      } else {
+         $('#permission1').val('')
+         $('#permission1').prop('disabled', true)
       }
    })
+   $('input[name=sapid2]').change(function () {
+      var sapid = $(this).val();
+      // console.log(sapid)
+      if (sapid !== '') {
+         $('#permission2').prop('disabled', false)
+         $('#permission2').prop('required', true)
+         $.ajax({  
+            url: "/check-sapid",
+            type: 'post',
+            data: {
+               sapid: sapid
+            },
+            success: function (result) {
+               // $('#idfrom').val(ui.item.unitid);
+               $('#username2').val(result.AccountName);
+               // console.log(result);
+            }
+         })
+      } else {
+         $('#permission2').val('')
+         $('#permission2').prop('disabled', true)
+         $('#permission2').prop('required', false)
+      }
+   })
+   $('input[name=sapid3]').change(function () {
+      var sapid = $(this).val();
+      // console.log(sapid)
+      if (sapid !== '') {
+         $('#permission3').prop('disabled', false)
+         $('#permission3').prop('required', true)
+         $.ajax({  
+            url: "/check-sapid",
+            type: 'post',
+            data: {
+               sapid: sapid
+            },
+            success: function (result) {
+               // $('#idfrom').val(ui.item.unitid);
+               $('#username3').val(result.AccountName);
+               // console.log(result);
+            }
+         })
+      } else {
+         $('#permission3').val('')
+         $('#permission3').prop('disabled', true)
+         $('#permission3').prop('required', false)
+      }
+   })
+   $('input[name=sapid4]').change(function () {
+      var sapid = $(this).val();
+      // console.log(sapid)
+      if (sapid !== '') {
+         $('#permission4').prop('disabled', false)
+         $('#permission4').prop('required', true)
+         $.ajax({  
+            url: "/check-sapid",
+            type: 'post',
+            data: {
+               sapid: sapid
+            },
+            success: function (result) {
+               // $('#idfrom').val(ui.item.unitid);
+               $('#username4').val(result.AccountName);
+               // console.log(result);
+            }
+         })
+      } else {
+         $('#permission4').val('')
+         $('#permission4').prop('disabled', true)
+         $('#permission4').prop('required', false)
+      }
+   })
+   $('input[name=sapid5]').change(function () {
+      var sapid = $(this).val();
+      // console.log(sapid)
+      if (sapid !== '') {
+         $('#permission5').prop('disabled', false)
+         $('#permission5').prop('required', true)
+         $.ajax({  
+            url: "/check-sapid",
+            type: 'post',
+            data: {
+               sapid: sapid
+            },
+            success: function (result) {
+               // $('#idfrom').val(ui.item.unitid);
+               $('#username5').val(result.AccountName);
+               // console.log(result);
+            }
+         })
+      } else {
+         $('#permission5').val('')
+         $('#permission5').prop('disabled', true)
+         $('#permission5').prop('required', false)
+      }
+   })
+   $('input[name=sapid6]').change(function () {
+      var sapid = $(this).val();
+      // console.log(sapid)
+      if (sapid !== '') {
+         $('#permission6').prop('disabled', false)
+         $('#permission6').prop('required', true)
+         $.ajax({  
+            url: "/check-sapid",
+            type: 'post',
+            data: {
+               sapid: sapid
+            },
+            success: function (result) {
+               // $('#idfrom').val(ui.item.unitid);
+               $('#username6').val(result.AccountName);
+               // console.log(result);
+            }
+         })
+      } else {
+         $('#permission6').val('')
+         $('#permission6').prop('disabled', true)
+         $('#permission6').prop('required', false)
+      }
+   })
+
+   // old input
+   var sapid1old = $('input[name=sapid1]').val()
+   let permission1old = $('#permission1').val()
+   // console.log(sapid1old);
+   if (sapid1old !== '') {
+      $('input[name=sapid1]').change();
+   }
+   if (permission1old !== '') {
+      $('#permission1').change();
+   }
+   var sapid2old = $('input[name=sapid2]').val()
+   let permission2old = $('#permission2').val()
+   // console.log(sapid1old);
+   if (sapid2old !== '') {
+      $('input[name=sapid2]').change();
+   }
+   if (permission2old !== '') {
+      $('#permission2').change();
+   }
+   var sapid3old = $('input[name=sapid3]').val()
+   let permission3old = $('#permission3').val()
+   // console.log(sapid1old);
+   if (sapid3old !== '') {
+      $('input[name=sapid3]').change();
+   }
+   if (permission3old !== '') {
+      $('#permission3').change();
+   }
+   var sapid4old = $('input[name=sapid4]').val()
+   let permission4old = $('#permission4').val()
+   // console.log(sapid1old);
+   if (sapid4old !== '') {
+      $('input[name=sapid4]').change();
+   }
+   if (permission4old !== '') {
+      $('#permission4').change();
+   }
+   var sapid5old = $('input[name=sapid5]').val()
+   let permission5old = $('#permission5').val()
+   // console.log(sapid1old);
+   if (sapid5old !== '') {
+      $('input[name=sapid5]').change();
+   }
+   if (permission5old !== '') {
+      $('#permission5').change();
+   }
+   var sapid6old = $('input[name=sapid6]').val()
+   let permission6old = $('#permission6').val()
+   // console.log(sapid1old);
+   if (sapid6old !== '') {
+      $('input[name=sapid6]').change();
+   }
+   if (permission6old !== '') {
+      $('#permission6').change();
+   }
 });
--- resources/views/admin/manage/addUser.blade.php
+++ resources/views/admin/manage/addUser.blade.php
@@ -17,28 +17,28 @@
       <section id="user1" class="grid grid-cols-1 lg:grid-cols-2 gap4">
           <div class="form-control w-full max-w-xs">
             <label class="label">
-              <span class="label-text text-slate-900 text-lg font-medium">รหัสพนักงาน SAPID</span>
+              <span class="label-text text-slate-900 text-lg font-medium">รหัสพนักงาน SAPID <b class="text-rose-600">*</b></span>
             </label>
-            <input type="text" placeholder="กรอกรหัสพนักงาน" name="sapid1" id="sapid1"
+            <input type="text" placeholder="กรอกรหัสพนักงาน" name="sapid1" id="sapid1" value="{{old('sapid1')}}" required
             class="input input-bordered w-full max-w-xs bg-white border-slate-400 text-lg font-medium" />
             <input type="text" id="username1" class="input input-bordered w-full max-w-xs disabled:bg-white disabled:border-white text-lg font-medium" placeholder="ชื่อผู้ใช้งาน" disabled>
           </div>
           <div class="form-control w-full max-w-xs">
             <label class="label">
-              <span class="label-text text-slate-900 text-lg font-medium">สิทธิ์ผู้ใช้งาน</span>
+              <span class="label-text text-slate-900 text-lg font-medium">สิทธิ์ผู้ใช้งาน <b class="text-rose-600">*</b></span>
             </label>
-            <select name="permission1" id="permission1" class="select select-bordered bg-white border-slate-400 text-lg font-normal">
+            <select disabled required name="permission1" id="permission1" class="select select-bordered disabled:bg-slate-200 bg-white border-slate-400 text-lg font-normal">
               <option value="" selected>---- เลือกสิทธิ์ผู้ใช้งาน ----</option>
-              <option value="1">ผู้ดูแลระบบ</option>
-              <option value="0">ผู้ใช้งานทั่วไป</option>
+              <option value="1" {{ (old("permission1") == "1" ? "selected": "") }}>ผู้ดูแลระบบ</option>
+              <option value="0" {{ (old("permission1") == "0" ? "selected": "") }}>ผู้ใช้งานทั่วไป</option>
             </select>
             @if($errors->has('massage1'))
-            <div class="alert alert-error shadow-lg">
+             <div class="alert alert-error shadow-lg mt-2">
                <div>
                  <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                  </svg>
-                 <span>{{ $errors->first('massage1') }}</span>
+                 <span class="text-base">{{ $errors->first('massage1') }}</span>
                </div>
              </div>
             @endif
@@ -50,7 +50,7 @@
             <label class="label">
               <span class="label-text text-slate-900 text-lg font-medium">รหัสพนักงาน SAPID</span>
             </label>
-            <input type="text" placeholder="กรอกรหัสพนักงาน" name="sapid2" id="sapid2"
+            <input type="text" placeholder="กรอกรหัสพนักงาน" name="sapid2" id="sapid2" value="{{old('sapid2')}}"
             class="input input-bordered w-full max-w-xs bg-white border-slate-400 text-lg font-medium" />
             <input type="text" id="username2" class="input input-bordered w-full max-w-xs disabled:bg-white disabled:border-white text-lg font-medium" placeholder="ชื่อผู้ใช้งาน" disabled>
           </div>
@@ -58,11 +58,21 @@
             <label class="label">
               <span class="label-text text-slate-900 text-lg font-medium">สิทธิ์ผู้ใช้งาน</span>
             </label>
-            <select name="permission2" id="permission2" class="select select-bordered bg-white border-slate-400 text-lg font-normal">
+            <select disabled name="permission2" id="permission2" class="select select-bordered disabled:bg-slate-200 bg-white border-slate-400 text-lg font-normal">
               <option value="" selected>---- เลือกสิทธิ์ผู้ใช้งาน ----</option>
-              <option value="1">ผู้ดูแลระบบ</option>
-              <option value="0">ผู้ใช้งานทั่วไป</option>
+              <option value="1" {{ (old("permission2") == "1" ? "selected": "") }}>ผู้ดูแลระบบ</option>
+              <option value="0" {{ (old("permission2") == "0" ? "selected": "") }}>ผู้ใช้งานทั่วไป</option>
             </select>
+            @if($errors->has('massage2'))
+             <div class="alert alert-error shadow-lg mt-2">
+               <div>
+                 <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                 </svg>
+                 <span class="text-base">{{ $errors->first('massage2') }}</span>
+               </div>
+             </div>
+            @endif
           </div>
       </section>
    
@@ -71,7 +81,7 @@
             <label class="label">
               <span class="label-text text-slate-900 text-lg font-medium">รหัสพนักงาน SAPID</span>
             </label>
-            <input type="text" placeholder="กรอกรหัสพนักงาน" name="sapid3" id="sapid3"
+            <input type="text" placeholder="กรอกรหัสพนักงาน" name="sapid3" id="sapid3" value="{{old('sapid3')}}"
             class="input input-bordered w-full max-w-xs bg-white border-slate-400 text-lg font-medium" />
             <input type="text" id="username3" class="input input-bordered w-full max-w-xs disabled:bg-white disabled:border-white text-lg font-medium" placeholder="ชื่อผู้ใช้งาน" disabled>
           </div>
@@ -79,11 +89,21 @@
             <label class="label">
               <span class="label-text text-slate-900 text-lg font-medium">สิทธิ์ผู้ใช้งาน</span>
             </label>
-            <select name="permission3" id="permission3" class="select select-bordered bg-white border-slate-400 text-lg font-normal">
+            <select disabled name="permission3" id="permission3" class="select select-bordered disabled:bg-slate-200 bg-white border-slate-400 text-lg font-normal">
               <option value="" selected>---- เลือกสิทธิ์ผู้ใช้งาน ----</option>
-              <option value="1">ผู้ดูแลระบบ</option>
-              <option value="0">ผู้ใช้งานทั่วไป</option>
+              <option value="1" {{ (old("permission3") == "1" ? "selected": "") }}>ผู้ดูแลระบบ</option>
+              <option value="0" {{ (old("permission3") == "0" ? "selected": "") }}>ผู้ใช้งานทั่วไป</option>
             </select>
+            @if($errors->has('massage3'))
+             <div class="alert alert-error shadow-lg mt-2">
+               <div>
+                 <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                 </svg>
+                 <span class="text-base">{{ $errors->first('massage3') }}</span>
+               </div>
+             </div>
+            @endif
           </div>
       </section>
    
@@ -92,7 +112,7 @@
             <label class="label">
               <span class="label-text text-slate-900 text-lg font-medium">รหัสพนักงาน SAPID</span>
             </label>
-            <input type="text" placeholder="กรอกรหัสพนักงาน" name="sapid4" id="sapid4"
+            <input type="text" placeholder="กรอกรหัสพนักงาน" name="sapid4" id="sapid4" value="{{old('sapid4')}}"
             class="input input-bordered w-full max-w-xs bg-white border-slate-400 text-lg font-medium" />
             <input type="text" id="username4" class="input input-bordered w-full max-w-xs disabled:bg-white disabled:border-white text-lg font-medium" placeholder="ชื่อผู้ใช้งาน" disabled>
           </div>
@@ -100,11 +120,21 @@
             <label class="label">
               <span class="label-text text-slate-900 text-lg font-medium">สิทธิ์ผู้ใช้งาน</span>
             </label>
-            <select name="permission4" id="permission4" class="select select-bordered bg-white border-slate-400 text-lg font-normal">
+            <select disabled name="permission4" id="permission4" class="select select-bordered disabled:bg-slate-200 bg-white border-slate-400 text-lg font-normal">
               <option value="" selected>---- เลือกสิทธิ์ผู้ใช้งาน ----</option>
-              <option value="1">ผู้ดูแลระบบ</option>
-              <option value="0">ผู้ใช้งานทั่วไป</option>
+              <option value="1" {{ (old("permission4") == "1" ? "selected": "") }}>ผู้ดูแลระบบ</option>
+              <option value="0" {{ (old("permission4") == "0" ? "selected": "") }}>ผู้ใช้งานทั่วไป</option>
             </select>
+            @if($errors->has('massage4'))
+             <div class="alert alert-error shadow-lg mt-2">
+               <div>
+                 <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                 </svg>
+                 <span class="text-base">{{ $errors->first('massage4') }}</span>
+               </div>
+             </div>
+            @endif
           </div>
       </section>
    
@@ -113,7 +143,7 @@
             <label class="label">
               <span class="label-text text-slate-900 text-lg font-medium">รหัสพนักงาน SAPID</span>
             </label>
-            <input type="text" placeholder="กรอกรหัสพนักงาน" name="sapid5" id="sapid5"
+            <input type="text" placeholder="กรอกรหัสพนักงาน" name="sapid5" id="sapid5" value="{{old('sapid5')}}"
             class="input input-bordered w-full max-w-xs bg-white border-slate-400 text-lg font-medium" />
             <input type="text" id="username5" class="input input-bordered w-full max-w-xs disabled:bg-white disabled:border-white text-lg font-medium" placeholder="ชื่อผู้ใช้งาน" disabled>
           </div>
@@ -121,11 +151,21 @@
             <label class="label">
               <span class="label-text text-slate-900 text-lg font-medium">สิทธิ์ผู้ใช้งาน</span>
             </label>
-            <select name="permission5" id="permission5" class="select select-bordered bg-white border-slate-400 text-lg font-normal">
+            <select disabled name="permission5" id="permission5" class="select select-bordered disabled:bg-slate-200 bg-white border-slate-400 text-lg font-normal">
               <option value="" selected>---- เลือกสิทธิ์ผู้ใช้งาน ----</option>
-              <option value="1">ผู้ดูแลระบบ</option>
-              <option value="0">ผู้ใช้งานทั่วไป</option>
+              <option value="1" {{ (old("permission5") == "1" ? "selected": "") }}>ผู้ดูแลระบบ</option>
+              <option value="0" {{ (old("permission5") == "0" ? "selected": "") }}>ผู้ใช้งานทั่วไป</option>
             </select>
+            @if($errors->has('massage5'))
+             <div class="alert alert-error shadow-lg mt-2">
+               <div>
+                 <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                 </svg>
+                 <span class="text-base">{{ $errors->first('massage5') }}</span>
+               </div>
+             </div>
+            @endif
           </div>
       </section>
    
@@ -134,7 +174,7 @@
             <label class="label">
               <span class="label-text text-slate-900 text-lg font-medium">รหัสพนักงาน SAPID</span>
             </label>
-            <input type="text" placeholder="กรอกรหัสพนักงาน" name="sapid6" id="sapid6"
+            <input type="text" placeholder="กรอกรหัสพนักงาน" name="sapid6" id="sapid6" value="{{old('sapid6')}}"
             class="input input-bordered w-full max-w-xs bg-white border-slate-400 text-lg font-medium" />
             <input type="text" id="username6" class="input input-bordered w-full max-w-xs disabled:bg-white disabled:border-white text-lg font-medium" placeholder="ชื่อผู้ใช้งาน" disabled>
           </div>
@@ -142,11 +182,21 @@
             <label class="label">
               <span class="label-text text-slate-900 text-lg font-medium">สิทธิ์ผู้ใช้งาน</span>
             </label>
-            <select name="permission6" id="permission6" class="select select-bordered bg-white border-slate-400 text-lg font-normal">
+            <select disabled name="permission6" id="permission6" class="select select-bordered disabled:bg-slate-200 bg-white border-slate-400 text-lg font-normal">
               <option value="" selected>---- เลือกสิทธิ์ผู้ใช้งาน ----</option>
-              <option value="1">ผู้ดูแลระบบ</option>
-              <option value="0">ผู้ใช้งานทั่วไป</option>
+              <option value="1" {{ (old("permission6") == "1" ? "selected": "") }}>ผู้ดูแลระบบ</option>
+              <option value="0" {{ (old("permission6") == "0" ? "selected": "") }}>ผู้ใช้งานทั่วไป</option>
             </select>
+            @if($errors->has('massage6'))
+             <div class="alert alert-error shadow-lg mt-2">
+               <div>
+                 <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                 </svg>
+                 <span class="text-base">{{ $errors->first('massage6') }}</span>
+               </div>
+             </div>
+            @endif
           </div>
       </section>
    
