1bcd41d7e49eb2705b875e9f4a23071e3040ed45
juntiwa <juntima.nuch@gmail.com>
2022-08-18 14:52:44 +0700

    add confirm to export file

--- app/Http/Controllers/Admin/HistoryController.php
+++ app/Http/Controllers/Admin/HistoryController.php
@@ -20,11 +20,6 @@ class HistoryController extends Controller
         $this->middleware(['auth', 'abilities', 'admin']);
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index(Request $request)
     {
         $logAvtivitys = LogActivity::orderby('date_time', 'desc')->paginate(50);
@@ -44,75 +39,36 @@ class HistoryController extends Controller
         return view('admin.history', ['logAvtivitys' => $logAvtivitys]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
     }
 
-    /**
-     * @return \Illuminate\Support\Collection
-     */
     public function export(Request $request)
     {
         $time_now = Carbon::now()->format('Y_m_d_H:i:s');
