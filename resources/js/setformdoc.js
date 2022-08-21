a7796d1194e9f0b51125ff2d924bde21931089ee
juntiwa <juntima.nuch@gmail.com>
2022-08-21 11:07:47 +0700

    fix js file

---
+++ resources/js/setformdoc.js
@@ -0,0 +1,7 @@
+function SetTt() {
+    // เมื่อกดและให้เป็น 0 เพื่อแสดงช่องกรอกข้อมูล
+    window.localStorage.setItem("tt", 0);
+    if (window.localStorage.getItem("tt") == 0) {
+        $("#inputSearch").slideDown();
+    }
+}
